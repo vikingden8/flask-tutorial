3dd32346776b4c81a66056c753b8b722af218677
Viking Den <vikingden7@gmail.com>
2018-11-29 23:04:04 +0800

    add authors

--- flask-introduction/library/_02_html_inside_view.py
+++ flask-introduction/library/_02_html_inside_view.py
@@ -8,8 +8,13 @@ def hello_world():
     html = """
         <html>
             <h1>Welcome to our Library!</h1>
+            {authors_list}
         </html>
     """
     authors = ["Alan Poe", "Jorge L. Borges", "Mark Twain"]
     # build an <ul> with authors
-    return html
+    authors_list = '<ul>'
+    authors_list += '\n'.join(['<li>{author}</li>'.format(author=author) for author in authors])
+    authors_list += '</ul>'
+
+    return html.format(authors_list=authors_list)
